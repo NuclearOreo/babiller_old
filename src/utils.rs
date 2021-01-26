e23418007503dd6fd46915779b23405593a58443
Ussama Azam <nuclearoreo@gmail.com>
2021-01-25 22:54:22 -0500

    Adding Utils with extract digits

---
+++ src/utils.rs
@@ -0,0 +1,13 @@
+pub(crate) fn extract_digits(s: &str) -> (&str, &str) {
+    (&s[1..], &s[0..1])
+}
+
+#[cfs(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn extract_one_digit() {
+        assert_eq!(extract_digits("1+2"), ("+2", "1"));
+    }
+}
