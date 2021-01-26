d80d2666f281843a7750fde6f08c0e1fa28869ee
Ussama Azam <nuclearoreo@gmail.com>
2021-01-25 22:59:06 -0500

    extract multiple digits

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,17 @@
 pub(crate) fn extract_digits(s: &str) -> (&str, &str) {
-    (&s[1..], &s[0..1])
+    let mut digits_end = 0;
+
+    for (idx, c) in s.char_indices() {
+        if c.is_ascii_digit() {
+            digits_end += 1;
+        } else {
+            break;
+        }
+    }
+
+    let digits = &s[..digits_end];
+    let remainder = &s[digits_end..];
+    (remainder, digits)
 }
 
 #[cfs(test)]
@@ -9,5 +21,10 @@ mod tests {
     #[test]
     fn extract_one_digit() {
         assert_eq!(extract_digits("1+2"), ("+2", "1"));
+    }
+
+    #[test]
+    fn extract_multiple_digit() {
+        assert_eq!(extract_digits("121-1323"), ("-1323", "121"));
     }
 }
