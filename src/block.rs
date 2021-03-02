a8d8072ef961421d765a8b28cbe9fb8adfbcb07b
Ussama Azam <nuclearoreo@gmail.com>
2021-03-01 22:48:45 -0500

    Adding statement tests

--- src/block.rs
+++ src/block.rs
@@ -8,8 +8,9 @@ pub struct Block {
 
 impl Block {
     pub fn new(s: &str) -> Result<(&str, Self), String> {
-        let s = utils::tag("{}", s)?;
-
+        let s = utils::tag("{", s)?;
+        let (s, _) = utils::extract_whitespace(s);
+        let s = utils::tag("}", s)?;
         Ok((s, Block { stmts: Vec::new() }))
     }
 }
@@ -17,9 +18,28 @@ impl Block {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::expr::{Expr, Number};
 
     #[test]
     fn parse_empty_block() {
         assert_eq!(Block::new("{}"), Ok(("", Block { stmts: Vec::new() })));
+    }
+
+    #[test]
+    fn parse_empty_block_with_whitespaces() {
+        assert_eq!(Block::new("{    }"), Ok(("", Block { stmts: Vec::new() })));
+    }
+
+    #[test]
+    fn parse_empty_block_with_one_statement() {
+        assert_eq!(
+            Block::new("{ 5 }"),
+            Ok((
+                "",
+                Block {
+                    stmts: vec![Stmt::Expr(Expr::Number(Number(5)))],
+                }
+            ))
+        );
     }
 }
